9fe2aa11719c68da37dcc71bf3b5270910f3ee08
youngbloodcyb <cameron.youngblood@gmail.com>
2025-05-08 23:17:42 -0700

    show organizations

--- app/(app)/d/page.tsx
+++ app/(app)/d/page.tsx
@@ -1,12 +1,21 @@
 import { Main } from "@/components/ds"
 import { getSession } from "@/lib/auth/server"
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
 
 export default async function Page() {
   const session = await getSession()
+  const orgs = await auth.api.listOrganizations({
+    headers: await headers(),
+  })
   return (
     <Main>
       <h1>Dashboard</h1>
       <h2>Current Organization: {session?.session.activeOrganizationId}</h2>
+      <h2>Organizations: {orgs.length}</h2>
+      {orgs.map((org) => (
+        <h2 key={org.id}>{org.name}</h2>
+      ))}
     </Main>
   )
 }
