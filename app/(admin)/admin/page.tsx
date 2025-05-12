cf3ae44c38332db8b34bd0e26ba8c5898ecb2285
youngbloodcyb <cameron.youngblood@gmail.com>
2025-05-12 11:20:20 -0700

    add craft to all pages

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,14 +1,18 @@
-import { Main } from "@/components/ds"
+import { Main, Section, Container } from "@/components/ds"
 import Link from "next/link"
 export default function Page() {
   return (
     <Main>
-      <h1>Admin</h1>
-      <p>Admin eyes only</p>
-      <div className="flex flex-col">
-        <Link href="/admin/users">Users</Link>
-        <Link href="/admin/organizations">Organizations</Link>
-      </div>
+      <Section>
+        <Container>
+          <h1>Admin</h1>
+          <p>Admin eyes only</p>
+          <div className="flex flex-col">
+            <Link href="/admin/users">Users</Link>
+            <Link href="/admin/organizations">Organizations</Link>
+          </div>
+        </Container>
+      </Section>
     </Main>
   )
 }
--- app/(app)/d/page.tsx
+++ app/(app)/d/page.tsx
@@ -1,4 +1,4 @@
-import { Main } from "@/components/ds"
+import { Main, Section, Container } from "@/components/ds"
 import { getSession } from "@/lib/auth/server"
 import { auth } from "@/lib/auth"
 import { headers } from "next/headers"
@@ -10,12 +10,16 @@ export default async function Page() {
   })
   return (
     <Main>
-      <h1>Dashboard</h1>
-      <h2>Current Organization: {session?.session.activeOrganizationId}</h2>
-      <h2>Organizations: {orgs.length}</h2>
-      {orgs.map((org) => (
-        <h2 key={org.id}>{org.name}</h2>
-      ))}
+      <Section>
+        <Container>
+          <h1>Dashboard</h1>
+          <h2>Current Organization: {session?.session.activeOrganizationId}</h2>
+          <h2>Organizations: {orgs.length}</h2>
+          {orgs.map((org) => (
+            <h2 key={org.id}>{org.name}</h2>
+          ))}
+        </Container>
+      </Section>
     </Main>
   )
 }
