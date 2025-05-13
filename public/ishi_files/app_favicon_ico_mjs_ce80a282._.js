864c5570db08645988ba0321a7077b09746eed21
Bridger Tower <bridgertower@gmail.com>
2025-05-13 13:43:40 -0600

    whoops

--- public/ishi_files/app_favicon_ico_mjs_ce80a282._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/4166a_next_dist_ee741e13._.js"
-],
-    source: "dynamic"
-});
