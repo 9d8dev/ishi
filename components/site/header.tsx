0bd1c43a1d6ac4cb4158cce9c50543ab144b6883
Bridger Tower <bridgertower@gmail.com>
2025-05-13 09:55:10 -0600

    add opengraph and twitte and logo

--- components/site/header.tsx
+++ components/site/header.tsx
@@ -1,9 +1,17 @@
 import { Nav } from "@/components/ds";
 
+import Image from "next/image";
+import Logo from "@/public/logo.svg";
+import LogoDark from "@/public/logo-dark.svg";
+import Link from "next/link";
+
 export const Header = () => {
   return (
     <Nav>
-      <p>Hello Header</p>
+      <Link href="/">
+        <Image src={Logo} alt="Logo" className="block dark:hidden" />
+        <Image src={LogoDark} alt="Logo" className="hidden dark:block" />
+      </Link>
     </Nav>
   );
 };
