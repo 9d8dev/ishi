64b4ae57a6def51e772520c276a19e3a0aa43068
Bridger Tower <bridgertower@gmail.com>
2025-05-13 11:26:32 -0600

    update logo component

--- components/site/logo.tsx
+++ components/site/logo.tsx
@@ -1,22 +1,31 @@
-import Image from "next/image";
 import LogoLight from "@/public/logo.svg";
 import LogoDark from "@/public/logo-dark-mode.svg";
+import Image from "next/image";
+import Link from "next/link";
 
-export const Logo = () => {
+export const Logo = ({
+  href = "/",
+  width = 72,
+  className,
+}: {
+  href?: string;
+  width?: number;
+  className?: string;
+}) => {
   return (
-    <>
+    <Link href={href} className={className}>
       <Image
-        width={72}
+        width={width}
         src={LogoLight}
         alt="Logo"
         className="block dark:hidden"
       />
       <Image
-        width={72}
+        width={width}
         src={LogoDark}
         alt="Logo"
         className="hidden dark:block"
       />
-    </>
+    </Link>
   );
 };
