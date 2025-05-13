35460644248788bbb45c7c7765955f5ca99f5d2e
Bridger Tower <bridgertower@gmail.com>
2025-05-13 09:58:10 -0600

    update nav

--- components/site/header.tsx
+++ components/site/header.tsx
@@ -1,17 +1,27 @@
+import { Button } from "@/components/ui/button";
 import { Nav } from "@/components/ds";
 
 import Image from "next/image";
 import Logo from "@/public/logo.svg";
-import LogoDark from "@/public/logo-dark.svg";
+import LogoDark from "@/public/logo-dark-mode.svg";
 import Link from "next/link";
 
 export const Header = () => {
   return (
-    <Nav>
+    <Nav
+      className="border-b bg-background sticky top-0 z-50"
+      containerClassName="flex justify-between items-center"
+    >
       <Link href="/">
-        <Image src={Logo} alt="Logo" className="block dark:hidden" />
-        <Image src={LogoDark} alt="Logo" className="hidden dark:block" />
+        <Image width={72} src={Logo} alt="Logo" className="block dark:hidden" />
+        <Image
+          width={72}
+          src={LogoDark}
+          alt="Logo"
+          className="hidden dark:block"
+        />
       </Link>
+      <Button>Sign In</Button>
     </Nav>
   );
 };
