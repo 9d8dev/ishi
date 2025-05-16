657613b6b17f4aa5fa2a8c63cd4b66998e13c8a5
youngbloodcyb <cameron.youngblood@gmail.com>
2025-05-15 23:53:25 -0700

    add getOrgs action

--- lib/data/organization.ts
+++ lib/data/organization.ts
@@ -2,6 +2,7 @@ import { auth } from "@/lib/auth"
 import { member, organization } from "@/lib/db/schema"
 import { db } from "@/lib/db"
 import { eq } from "drizzle-orm"
+import { getSession } from "@/lib/auth/server"
 
 export const createOrganization = async (userId: string, email: string) => {
   try {
@@ -26,4 +27,18 @@ export const getActiveOrganization = async (userId: string) => {
     .limit(1)
 
   return org.organization
+}
+
+export const getOrganizations = async () => {
+  const session = await getSession()
+  if (!session) {
+    throw new Error("Unauthenticated")
+  }
+
+  if (session?.user.role !== "admin") {
+    throw new Error("Unauthorized")
+  }
+
+  const orgs = await db.select().from(organization)
+  return orgs
 }
